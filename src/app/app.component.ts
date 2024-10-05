89da43f89eb3541b362a0855e849cd5759905f3b
Jobi S S <jobisjobi1234@gmail.com>
2024-10-05 06:28:31 +0530

    added vercel analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./components/home/home.component";
+import { inject } from "@vercel/analytics"
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,9 @@ import { HomeComponent } from "./components/home/home.component";
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'jobi-portfolio';
+  ngOnInit(): void {
+    inject(); // Initialize Vercel Analytics here
+  }
 }
