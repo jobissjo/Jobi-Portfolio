8ae4738535d65db802ab6940d3459e5a005a14e5
Jobi S S <jobisjobi1234@gmail.com>
2024-12-24 12:23:35 +0530

    animate in know about me and job title typing style

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,13 +1,39 @@
-import { Component, output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, output } from '@angular/core';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
-export class MainComponent {
+export class MainComponent implements OnInit{
   
- 
+  text:string= 'Python Developer';
+  displayedText = '';
+  typingSpeed = 150;
+
+  ngOnInit() {
+    this.typeJobTitle();
+  }
+
+  typeJobTitle(){
+    let currentIndex = 0;
+    const interval = setInterval(()=> {
+
+      if (currentIndex < this.text.length){
+        if (this.text[currentIndex] == ' '){
+          this.displayedText += '<br />'
+        }else{
+          this.displayedText += this.text[currentIndex];
+        }
+        
+        currentIndex++;
+      }else{
+        clearInterval(interval);
+        
+      }
+    }, this.typingSpeed)
+  }
 }
