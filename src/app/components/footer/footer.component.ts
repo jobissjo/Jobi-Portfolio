2bfec5b84c692ff71550f88513e15861a8c475fe
Jobi S S <jobisjobi1234@gmail.com>
2025-12-13 23:20:02 +0530

    footer dark mode toggle

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-
+  @Input() darkMode: boolean = false;
 }
